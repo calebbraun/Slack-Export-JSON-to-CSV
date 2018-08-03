a1c0f8f9b7a35006f1d5e08ea1f3e542c411415f
calebbraun <calebjbraun@gmail.com>
2018-08-02 20:16:55 -0400

    Add comments and more functions

--- slack_json_to_csv.py
+++ slack_json_to_csv.py
@@ -1,6 +1,16 @@
-import json, sys
-import os, csv
-from pprint import pprint
+"""
+Convert Slack messages exported in their complicated JSON format to simple CSV format.
+
+To run the application use the following command:
+
+python slack_json_to_csv.py channel_export_dir path_to_slack_users.json output.csv
+eg.
+python slack_json_to_csv.py slack_export/channelA slack_export/users.json output.csv
+"""
+import sys
+import os
+import csv
+import json
 import re
 from datetime import datetime
 
@@ -8,10 +18,10 @@ from datetime import datetime
 def handle_annotated_mention(matchobj):
     return "@{}".format((matchobj.group(0)[2:-1]).split("|")[1])
 
+
 def handle_mention(matchobj):
-    global user
-    print(user[matchobj.group(0)[2:-1]][0])
-    return "@{}".format(user[matchobj.group(0)[2:-1]][0])
+    global users
+    return "@{}".format(users[matchobj.group(0)[2:-1]][0])
 
 
 def transform_text(text):
@@ -23,38 +33,99 @@ def transform_text(text):
     text = re.compile("<@U\w+>").sub(handle_mention, text)
     return text
 
-jsondir = sys.argv[1]
-userjson = sys.argv[2]
-outcsv_file = sys.argv[3]
 
-content_list = []
-userlist  = []
-f = open(outcsv_file, 'w')
-user = {};
-with open(userjson) as user_data:
-    userlist = json.load(user_data)
-    for userdata in userlist:
-        userid = userdata["id"]
-        if "real_name" in userdata and userdata["real_name"]:
-            realname = userdata["real_name"]
-            if not re.match('.*[a-zA-Z].*', realname) :
-                realname = userdata["name"]
-        else:
-            realname = userdata["name"]
-        print(realname)
-        user[userid] = [realname]
-csvwriter = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-for content in os.listdir(jsondir):
-    content_list.append(content)
-    with open(jsondir + '/' + content) as data_file:
-        data = json.load(data_file)
-        for item in data:
-            if item["type"] == "message" :
-                if item["text"].find("> has joined the channel") == -1:
-                    user_cur = user[item["user"]]
-                    ts = datetime.utcfromtimestamp(float(item['ts']))
-                    time = ts.strftime("%Y-%m-%d %H:%M:%S")
-                    item["text"] = transform_text(item["text"])
-                    csvwriter.writerow([time.encode('utf-8'),item['text'].encode('utf-8'),user_cur[0].encode('utf-8')])
+def check_exist(pth, type):
+    """
+    Check the existence of the input file path.
 
-f.close()
+    :param pth:         Full path to directory or file
+    :param type:        One of 'directory' or 'file'
+    :return:            Validated full path to directory
+    """
+    if type == 'directory' and os.path.isdir(pth):
+        return pth
+    elif os.path.isfile(pth):
+        return pth
+    else:
+        raise IOError('Input {} does not exist:  {}'.format(type, pth))
+
+
+def get_all_users(in_json):
+    """
+    Read the users.json file.
+
+    :param in_json:     Full path to the json file
+    :return:            Parsed users json
+    """
+    print("Users: ")
+    with open(in_json) as user_data:
+        user_json = json.load(user_data)
+        users = {}  # Dictionary to remember all users
+
+        # Slackbot is not in users file -- add manually
+        users["USLACKBOT"] = "slackbot"
+
+        for usr in user_json:
+            userid = usr["id"]
+            if "real_name" in usr and usr["real_name"]:
+                realname = usr["real_name"]
+                if not re.match('.*[a-zA-Z].*', realname):
+                    realname = usr["name"]
+            else:
+                realname = usr["name"]
+
+            print("\t{}".format(realname))
+            users[userid] = realname
+
+        return users
+
+
+def write_message(csvwriter, message, author):
+    """
+    Write a single message from the user json as a row in the output csv.
+
+    :param csvwriter:   A csv writer object
+    :param message:     A dictionary containing information about one message
+    :param author:      The name of the message author
+    """
+    ts = datetime.utcfromtimestamp(float(message['ts']))
+    time = ts.strftime("%Y-%m-%d %H:%M:%S")
+    message = transform_text(message["text"])
+
+    csvwriter.writerow([time, author, message])
+
+
+# Make sure inputs exist
+json_dir = check_exist(sys.argv[1], 'directory')
+userjson = check_exist(sys.argv[2], 'file')
+outcsv = sys.argv[3]
+
+users = get_all_users(userjson)
+
+all_messages = []
+
+for day_json in os.listdir(json_dir):
+    # Open a json file containing all messages for a single day
+    with open(json_dir + '/' + day_json) as data_file:
+        day = json.load(data_file)
+        for message in day:
+            if message["type"] == "message":
+                # Don't record channel joins
+                if "subtype" in message and message["subtype"] == "channel_join":
+                    continue
+
+                author = users[message["user"]]
+
+                ts = datetime.utcfromtimestamp(float(message['ts']))
+                message = transform_text(message["text"])
+                all_messages.append([author, ts, message])
+
+# Sort all the messages by the timestamp, then make time more readable
+all_messages = sorted(all_messages, key=lambda x: x[1])
+all_messages = [[x[0], x[1].strftime("%Y-%m-%d"), x[2]] for x in all_messages]
+all_messages = [[x.encode('utf-8') for x in y] for y in all_messages]
+all_messages = [[x.encode('ascii', 'ignore') for x in y] for y in all_messages]
+
+with open(outcsv, 'w') as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerows(all_messages)
